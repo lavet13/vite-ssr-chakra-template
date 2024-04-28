87154631a4e5b06eadc673bbe98d7b7063d238f1
Ivan <lavet13@mail.ru>
2024-04-28 18:07:00 +0300

    fixed issue with building the project

--- vite.config.ts
+++ vite.config.ts
@@ -27,15 +27,7 @@ export default defineConfig(({ command, mode }) => {
       rollupOptions: {
         input: './src/main.tsx',
         output: {
-          manualChunks(id: string) {
-            if (id.includes('node_modules')) {
-              return id
-                .toString()
-                .split('node_modules/')[1]
-                .split('/')[0]
-                .toString();
-            }
-          },
+          manualChunks: undefined,
         },
       },
     },
